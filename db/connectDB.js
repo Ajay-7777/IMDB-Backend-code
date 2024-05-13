24eb49705c74759654a81af7030e8eda139b5f52
Ajay-7777 <65538820+Ajay-7777@users.noreply.github.com>
2024-05-13 15:51:35 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+let url=`mongodb+srv://akkirahi810:abcd1234@cluster0.tkbwrnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const connectDB = (url) => {
     return mongoose.connect(url)
 }
